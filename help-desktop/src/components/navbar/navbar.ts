8123389e32f81024511419b94b4776a828180e49
Jake24012001 <oberinho2401@outlook.com>
2025-09-11 22:04:16 -0500

    cambios en css y html navbar

--- help-desktop/src/components/navbar/navbar.ts
+++ help-desktop/src/components/navbar/navbar.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone:true,
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  isAdmin = false;
 }
