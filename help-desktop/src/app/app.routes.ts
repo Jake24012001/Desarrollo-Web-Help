7cc0bf44e4a74996f59b9e9114602398ffadf4bf
Jake24012001 <oberinho2401@outlook.com>
2025-09-11 19:55:12 -0500

    cambios en html

--- help-desktop/src/app/app.routes.ts
+++ help-desktop/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Navbar } from '../components/navbar/navbar';
+import { VistaPrincipal } from '../components/vista-principal/vista-principal';
+import { Footer } from '../components/footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Navbar},
+    {path:'help-menu',component:VistaPrincipal,
+        children:[
+            {
+                path:'footer',component:Footer
+            }
+        ]
+    }
+];
