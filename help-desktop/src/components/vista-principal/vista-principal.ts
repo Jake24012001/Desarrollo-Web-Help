0cc5632072e548c8ca7dd27d425b4230a6306a42
Jake24012001 <oberinho2401@outlook.com>
2025-09-11 19:45:20 -0500

    todos los datos que van a estar disponibles

---
+++ help-desktop/src/components/vista-principal/vista-principal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-principal',
+  imports: [],
+  templateUrl: './vista-principal.html',
+  styleUrl: './vista-principal.css'
+})
+export class VistaPrincipal {
+
+}
