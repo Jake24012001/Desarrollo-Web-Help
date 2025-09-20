74411a5d0cd4b0d4dc601cfabae8e6d29a9e7ca5
Jake24012001 <oberinho2401@outlook.com>
2025-09-19 21:21:18 -0500

    cambios

--- help-desktop/src/components/vista-principal/vista-principal.ts
+++ help-desktop/src/components/vista-principal/vista-principal.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-vista-principal',
@@ -8,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './vista-principal.css',
 })
 export class VistaPrincipal {
-  
+  crearPeticion(): void {
+    Swal.fire({
+      title: 'Petición creada',
+      text: 'Tu solicitud ha sido registrada correctamente.',
+      icon: 'success',
+      confirmButtonText: 'Cool',
+    });
+  }
+
+  modificarPeticion(): void {
+    Swal.fire({
+      title: 'Modificar petición',
+      text: '¿Deseas editar esta solicitud?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'No',
+    });
+  }
+
+  borrarPeticion(): void {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Esta acción eliminará la petición permanentemente.',
+      icon: 'error',
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar',
+    });
+  }
 }
