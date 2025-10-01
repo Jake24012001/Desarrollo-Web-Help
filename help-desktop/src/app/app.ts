5477e9adb2fe88ba252865aeb4d332228d2b9eb1
Jake24012001 <oberinho2401@outlook.com>
2025-09-30 21:01:29 -0500

    cambios
    cositas

--- help-desktop/src/app/app.ts
+++ help-desktop/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "../components/navbar/navbar";
 import { Footer } from "../components/footer/footer";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer,HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
