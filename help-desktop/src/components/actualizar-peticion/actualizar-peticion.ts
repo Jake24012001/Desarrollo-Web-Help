a2f6b323ea946615a3d0e4bc4f3c5ca1e1a0c060
Jake24012001 <oberinho2401@outlook.com>
2025-10-03 22:51:21 -0500

    cargar datos de ticket

--- help-desktop/src/components/actualizar-peticion/actualizar-peticion.ts
+++ help-desktop/src/components/actualizar-peticion/actualizar-peticion.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TicketService } from '../../app/services/ticket.service';
+import { Ticket } from '../../interface/Ticket';
 
 @Component({
   selector: 'app-actualizar-peticion',
-  standalone:true,
+  standalone: true,
   imports: [],
   templateUrl: './actualizar-peticion.html',
-  styleUrl: './actualizar-peticion.css'
+  styleUrl: './actualizar-peticion.css',
 })
-export class ActualizarPeticion{
+export class ActualizarPeticion implements OnInit {
+  //imports de todos los datos que necesito
+  serviciotickes: TicketService[] = [];
+  datosticket: Ticket[] = [];
 
-  actualizarTicket(){
-   
+  constructor(private servicesticket: TicketService) {}
 
-
-
+  ngOnInit(): void {
+    // se importa toda la informacion del ticke seleccionado
+    this.cargarDatos();
   }
 
+  cargarDatos(): void {
+    this.servicesticket.getAll().subscribe((datos) => {
+      this.datosticket = datos;
+    });
+  }
 }
